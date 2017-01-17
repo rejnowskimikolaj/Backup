6e44217edab298da665a3bb4d27c72fc69117d29
mikolaj <rejnowski.mikolaj@gmail.com>
2017-01-17 20:51:26 +0100

    quizer update 17-01

--- Quizer2/Quizer/app/src/main/java/com/example/user/quizer/GameActivity.java
+++ Quizer2/Quizer/app/src/main/java/com/example/user/quizer/GameActivity.java
@@ -1,9 +1,18 @@
 package com.example.user.quizer;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -17,23 +26,38 @@ public class GameActivity extends AppCompatActivity {
 
     public static final String QUESTION_TAG = "question";
     private List<Question> questions;
+    private ListView levelListView;
+    AlertDialog levelAlert;
+
+    private List<String> levelList;
+
     QuestionFragment questionFragment;
     GameInfoFragment gameInfoFragment;
     LifeBeltFragment lifeBeltFragment;
     private int questionNumber;
     final private int QUESTIONMAXDIFFICULTY=14;
 
+    public void setLevelList(){
+        levelList = new ArrayList<>();
+        int [] arr = {100,200,300,500,1000,2000,4000,8000,16000,32000,64000,125000,500000,1000000};
+
+        for(int i:arr){
+            levelList.add(i+"");
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+
+        setLevelList();
         questions = new ArrayList<>();
         QuestionLoader questionLoader= new QuestionLoader(R.raw.questions,this);
         questionLoader.loadQuestions();
         questions=questionLoader.getQuestionList();
 
-        Log.d("QUESTIONSLIST", questions.size()+"");
+       // Log.d("QUESTIONSLIST", questions.size()+"");
 
         Bundle questionArguments = new Bundle();
         questionArguments.putParcelable(QUESTION_TAG,getDifficultyQuestion(questionNumber));
@@ -77,6 +101,8 @@ public class GameActivity extends AppCompatActivity {
 
             else {
                 ///NEXT QUESTION
+
+                displayDialog(questionNumber);
                 questionNumber++;
                 Bundle questionArguments = new Bundle();
                 questionArguments.putParcelable(QUESTION_TAG, getDifficultyQuestion(questionNumber));
@@ -153,4 +179,39 @@ public class GameActivity extends AppCompatActivity {
         public void useFiftyLifebelt() {
             this.questionFragment.useFiftyLifebelt();
         }
+
+        public void displayDialog(final int level){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            levelListView = new ListView(this);
+
+            ArrayAdapter adapter = new ArrayAdapter(this,R.layout.level_dialog_item,levelList) {
+
+
+
+                @NonNull
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent) {
+                    TextView textView = (TextView) super.getView(position, convertView, parent);
+
+
+                    textView.setText(levelList.get(position));
+                    if(position<=level) {
+                        textView.setTextColor(getResources().getColor(R.color.green));
+                    }
+                    return textView;
+                }
+            };
+
+
+
+            levelListView.setAdapter(adapter);
+
+
+            builder.setView(levelListView);
+            levelAlert = builder.create();
+            levelAlert.show();
+        }
+
 }
+
